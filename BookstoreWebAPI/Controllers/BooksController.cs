ae66ec42f8bf1bbb5acd67c73c78fa0cce14ab88
jawad <jawadhayatshreefmughal@gmail.com>
2023-09-20 09:49:40 +0500

    Add project files.

---
+++ BookstoreWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,54 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookstoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public async Task<ActionResult<List<Books>>> Get()
+        {
+            var books = await _db.Books.ToListAsync();
+            return books;
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Books>> Get(int id)
+        {
+            return await _db.Books.FindAsync(id);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
